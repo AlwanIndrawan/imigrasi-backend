8e668b02fcbcbe181339a7f848b1d0fbcacd2983
Alwan Indrawan Azis <alwaanindrawan@gmail.com>
2026-05-18 14:30:48 +0800

    ganti model

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post("/chat", async (req, res) => {
     const userText = req.body.message;
 
     const response = await axios.post(
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
   {
     contents: [
       {
